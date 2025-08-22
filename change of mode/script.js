ef8500202d6fa4000fc1efd461f3f4ffd1b4b973
Dheeraj Kumar <dheerajraut4902@gmail.com>
2025-08-22 21:54:30 +0530

    first commit

---
+++ change of mode/script.js
@@ -0,0 +1,13 @@
+let mode = document.querySelector("#mode");
+let currMode = "light";
+
+mode.addEventListener("click", () => {
+  if (currMode === "light") {
+    currMode = "dark";
+    document.querySelector("body").style.backgroundColor = "black";
+  } else {
+    currMode = "light";
+    document.querySelector("body").style.backgroundColor = "white";
+  }
+  console.log(currMode);
+});
